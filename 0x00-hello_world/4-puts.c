6aa76ae89687e9ca9a0593abf2ebbe91ee5ba967
odunayo22 <shakhordun@gmail.com>
2022-10-13 05:04:40 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-	char str[] = "Programming is like building a multilingual puzzle\n;
+	char str[] = "Programming is like building a multilingual puzzle\n";
 
         puts(str);	        
 
