98a95cf0d470bf329a09302b45183bd98be896a4
odunayo22 <shakhordun@gmail.com>
2022-10-13 09:03:24 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-	int integerType;
-	float floatType;
-        double doubleType;
-	char charType;
-        printf("Size of int: %ld bytes\n", sizeof(integerType));
-	printf("Size of float: %ld bytes\n", sizeof(floatType));
-        printf("Size of double: %ld bytes\n", sizeof(doubleType));
-        printf("Size of char: %ld byte\n", sizeof(charType));
-        return 0;
+int integerType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %ld bytes\n", sizeof(integerType));
+printf("Size of float: %ld bytes\n", sizeof(floatType));
+printf("Size of double: %ld bytes\n", sizeof(doubleType));
+printf("Size of char: %ld byte\n", sizeof(charType));
+return (0);
+
 }
