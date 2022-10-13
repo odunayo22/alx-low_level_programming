b6b103b75cc3c37c6c0183c62e7db5b76920c8ce
odunayo22 <shakhordun@gmail.com>
2022-10-13 05:43:28 -0700

    task4-corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
+//displaying text
+char str[] = "Programming is like building a multilingual puzzle\n";        	puts(str);	       
+return (0);
 
-        puts(str);	        
-
-	return 0;
 }
 
