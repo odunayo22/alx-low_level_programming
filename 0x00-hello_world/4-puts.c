521911ad13cfc7f25e2005e8f8ba57ed6949fe0c
odunayo22 <shakhordun@gmail.com>
2022-10-14 04:50:03 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #inlude <stdio.h>
-/** * main - Prints "Programming is like building a multilingual * puzzle, followed by a new line.* * Return:Always 0. */
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-put("\"Programming is like building a multilingual puzzle";        	
-return (0);
+	puts("\"Programming is like building a multilingual puzzle";        	
+	return (0);
 }
 
