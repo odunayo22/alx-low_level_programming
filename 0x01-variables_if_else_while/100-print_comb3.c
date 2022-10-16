e7642e863b0382afdf1f07a162ead8ccae9eb3b2
odunayo22 <shakhordun@gmail.com>
2022-10-16 04:39:13 -0700

    additional-task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
 /**
- *main
+ *main - print combination of numbers with no duplicate numbers
+ *
  *Return: Always 0 (Success)
  */
 
 int main(void)
+{
+	int ones;
+	int tens;
 
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /*ones tens+1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
