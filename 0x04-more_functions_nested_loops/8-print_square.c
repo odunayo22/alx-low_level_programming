f8637cdd5eca55dc7ab2b7a8710779deb071a899
odunayo22 <shakhordun@gmail.com>
2022-10-20 07:11:12 -0700

    task8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,8 +11,20 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	} else
-	
 
-									{
+	{
+		int i, j;
 
-											
+		for (i = 0; i < size; i++)
+		{
+
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+
+			}
+
+			_putchar('\n');
+		}
+	}
+}
