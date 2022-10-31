c9d9e3bc1e4ef18c5258fd427659f40740426ffc
odunayo22 <shakhordun@gmail.com>
2022-10-31 09:34:14 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * haystack: input
+ * needle: input
+ * Return Always 0 Success
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
