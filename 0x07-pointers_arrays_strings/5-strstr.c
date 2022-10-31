20b2f801c14ba79c869cdd792cfe5d6745d65e5f
odunayo22 <shakhordun@gmail.com>
2022-10-31 12:45:07 -0700

    pointer7

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,9 +2,9 @@
 
 /**
  * _strstr - Entry point
- * haystack: input
- * needle: input
- * Return Always 0 Success
+ *@haystack: input
+ *@needle: input
+ *Return Always 0 (Success)
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -14,3 +14,15 @@ char *_strstr(char *haystack, char *needle)
 		char *l = haystack;
 		char *p = needle;
 
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
