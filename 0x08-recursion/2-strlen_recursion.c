275eedf3ddad920d2666f97c6f5ea60d3e6cf93b
odunayo22 <shakhordun@gmail.com>
2022-11-02 08:27:55 -0700

    recursion-task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,3 +5,15 @@
  * @s :The string to be measured
  * Return - The length of the string
  */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
