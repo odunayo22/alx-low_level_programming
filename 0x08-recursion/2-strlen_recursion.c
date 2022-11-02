318ff3ef8438df1508027b2d63daa7ae2c3bd6d9
odunayo22 <shakhordun@gmail.com>
2022-11-02 08:01:59 -0700

    recursion-tasks

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- function that returns the length of a string.
+ * @s :The string to be measured
+ * Return - The length of the string
+ */
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial- returns the factorial of a  number
+ * @n: mumber to return factorial from
+ * Return- factorial from n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
