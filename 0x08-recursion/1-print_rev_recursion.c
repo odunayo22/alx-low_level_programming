c9528ed3315bbfb7b53627f86984e504c0690d49
odunayo22 <shakhordun@gmail.com>
2022-11-02 06:27:14 -0700

    recursion-task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - Prints a string in reverse
+ * @s: string to be printed
+ * Return - Always 0 (success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
