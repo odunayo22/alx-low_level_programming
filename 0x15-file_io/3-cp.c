bb84e88f3acba0b8c40e561584ccd133134b44c4
odunayo22 <shakhordun@gmail.com>
2022-12-09 08:55:04 -0800

    3-cp.c-correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,8 +10,8 @@
 int main(int argc, char **argv)
 {
 	int f1, f2, n;
-        char buf[1024];
-	
+	char buf[1024];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -21,31 +21,31 @@ int main(int argc, char **argv)
 	if (f1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	        exit(98);
+		exit(98);
 	}
 	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	while ((n = read(f1, buf, 1024)) > 0)
-        {
+	{
 		if (write(f2, buf, n) != n || f2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	                exit(99);
-	        }
+			exit(99);
+		}
 	}
 	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-        }
+	}
 	if (close(f1) < 0)
 	{
-	        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-	        exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
+		exit(100);
 	}
 	if (close(f2) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
 		exit(100);
-        }
+	}
 	return (0);
 }
